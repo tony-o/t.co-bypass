9f0d912c489384d472c8d9c2a85c9c27663ac085
Tim Dorr <timdorr@timdorr.com>
2014-07-23 10:25:31 -0400

    Fix up that gnarly nested ternary.

--- greasemonkey.user.js
+++ greasemonkey.user.js
@@ -12,14 +12,21 @@ var observer = new MutationObserver(function(mutations) {
   var aTags = document.body.getElementsByTagName("a");
   for (var i = 0; i < aTags.length; i++) {
     var tag = aTags[i];
-    if(tag.getAttribute("class") !== null && tag.getAttribute("class").indexOf("twitter-atreply") > -1){
+    
+    if (tag.getAttribute("class") !== null && tag.getAttribute("class").indexOf("twitter-atreply") > -1) {
       continue;
     }
+
     if (tag.href && tag.href.indexOf("://t.co/") > -1) {
-      tag.href = tag.getAttribute("data-expanded-url") !== "" && tag.getAttribute("data-expanded-url") !== null ? tag.getAttribute("data-expanded-url") : tag.getAttribute("data-full-url") !== "" && tag.getAttribute("data-full-url") !== null ? tag.getAttribute("data-full-url") : tag.getAttribute("title");
+      if (tag.getAttribute("data-expanded-url") !== "" && tag.getAttribute("data-expanded-url") !== null) {
+        tag.href = tag.getAttribute("data-expanded-url");
+      } else if (tag.getAttribute("data-full-url") !== "" && tag.getAttribute("data-full-url") !== null) {
+        tag.href = tag.getAttribute("data-full-url");
+      } else {
+        tag.href = tag.getAttribute("title");
+      }
     }
   };
-
 });
 
 var config = { 
