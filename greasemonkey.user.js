5d9bc729ef1cd151c6a83ec5fee56b31b340c096
Tony O'Dell <tony.odell@live.com>
2014-07-22 22:39:24 -0700

    fixing script to use one source file, fixing replies that kept generating randomly long #s after the link and eventually making the browser unusable

--- greasemonkey.user.js
+++ greasemonkey.user.js
@@ -12,7 +12,9 @@ var observer = new MutationObserver(function(mutations) {
   var aTags = document.body.getElementsByTagName("a");
   for (var i = 0; i < aTags.length; i++) {
     var tag = aTags[i];
-
+    if(tag.getAttribute("class") !== null && tag.getAttribute("class").indexOf("twitter-atreply") > -1){
+      continue;
+    }
     if (tag.href && tag.href.indexOf("://t.co/") > -1) {
       tag.href = tag.getAttribute("data-expanded-url") !== "" && tag.getAttribute("data-expanded-url") !== null ? tag.getAttribute("data-expanded-url") : tag.getAttribute("data-full-url") !== "" && tag.getAttribute("data-full-url") !== null ? tag.getAttribute("data-full-url") : tag.getAttribute("title");
     }
