74b3316f75965f8eb6bf7b0ce9606163baddbd2e
Tony O'Dell <tony.odell@live.com>
2014-07-22 22:27:19 -0700

    fixing greasemonkey for tweetdeck

--- greasemonkey.user.js
+++ greasemonkey.user.js
@@ -14,7 +14,7 @@ var observer = new MutationObserver(function(mutations) {
     var tag = aTags[i];
 
     if (tag.href && tag.href.indexOf("://t.co/") > -1) {
-      tag.href = tag.getAttribute("data-expanded-url") !== "" ? tag.getAttribute("data-expanded-url") : tag.getAttribute("title");
+      tag.href = tag.getAttribute("data-expanded-url") !== "" && tag.getAttribute("data-expanded-url") !== null ? tag.getAttribute("data-expanded-url") : tag.getAttribute("data-full-url") !== "" && tag.getAttribute("data-full-url") !== null ? tag.getAttribute("data-full-url") : tag.getAttribute("title");
     }
   };
 
