d96a5dc397d7ed9af66a85dda630d7b842cb0183
Samat K Jain <samat@samat.org>
2014-07-24 14:48:01 -0600

    Add Content Scope Runner
    [CSP][1] is required for user script managers that don't, by default,
    run in the scope of the page (e.g. pre-2.0 GreaseMonkey, Scriptish,
    Opera)
    
     [1]: http://wiki.greasespot.net/Content_Scope_Runner

--- greasemonkey.user.js
+++ greasemonkey.user.js
@@ -4,34 +4,61 @@
 // @include     https://twitter.com/*
 // @include     https://tweetdeck.com/*
 // @include     https://tweetdeck.twitter.com/*
-// @version     1
+// @version     2
 // @grant       none
 // ==/UserScript==
 
-var observer = new MutationObserver(function(mutations) {
-  var aTags = document.body.getElementsByTagName("a");
-  for (var i = 0; i < aTags.length; i++) {
-    var tag = aTags[i];
-    
-    if (tag.getAttribute("class") !== null && tag.getAttribute("class").indexOf("twitter-atreply") > -1) {
-      continue;
-    }
+if ('undefined' == typeof __PAGE_SCOPE_RUN__) {
+  (function page_scope_runner() {
+    // If we're _not_ already running in the page, grab the full source
+    // of this script.
+    var my_src = "(" + page_scope_runner.caller.toString() + ")();";
 
-    if (tag.href && tag.href.indexOf("://t.co/") > -1) {
-      if (tag.getAttribute("data-expanded-url") !== "" && tag.getAttribute("data-expanded-url") !== null) {
-        tag.href = tag.getAttribute("data-expanded-url");
-      } else if (tag.getAttribute("data-full-url") !== "" && tag.getAttribute("data-full-url") !== null) {
-        tag.href = tag.getAttribute("data-full-url");
-      } else {
-        tag.href = tag.getAttribute("title");
-      }
-    }
-  };
-});
+    // Create a script node holding this script, plus a marker that lets us
+    // know we are running in the page scope (not the Greasemonkey sandbox).
+    // Note that we are intentionally *not* scope-wrapping here.
+    var script = document.createElement('script');
+    script.setAttribute("type", "application/javascript");
+    script.textContent = "var __PAGE_SCOPE_RUN__ = true;\n" + my_src;
 
-var config = { 
-  childList: true,
-  subtree: true
-};
+    // Insert the script node into the page, so it will run, and immediately
+    // remove it to clean up.  Use setTimeout to force execution "outside" of
+    // the user script scope completely.
+    window.setTimeout(function() {
+          document.body.appendChild(script);
+          //document.body.removeChild(script);
+        }, 0);
+  })();
 
-observer.observe(document.body, config);
+  // Stop running, because we know Greasemonkey actually runs us in
+  // an anonymous wrapper.
+  return;
+} else {
+	var observer = new MutationObserver(function(mutations) {
+		var aTags = document.body.getElementsByTagName("a");
+		for (var i = 0; i < aTags.length; i++) {
+			var tag = aTags[i];
+
+			if (tag.getAttribute("class") !== null && tag.getAttribute("class").indexOf("twitter-atreply") > -1) {
+				continue;
+			}
+
+			if (tag.href && tag.href.indexOf("://t.co/") > -1) {
+				if (tag.getAttribute("data-expanded-url") !== "" && tag.getAttribute("data-expanded-url") !== null) {
+					tag.href = tag.getAttribute("data-expanded-url");
+				} else if (tag.getAttribute("data-full-url") !== "" && tag.getAttribute("data-full-url") !== null) {
+					tag.href = tag.getAttribute("data-full-url");
+				} else {
+					tag.href = tag.getAttribute("title");
+				}
+			}
+		};
+	});
+
+	var config = {
+		childList: true,
+		subtree: true
+	};
+
+	observer.observe(document.body, config);
+}
